16dacf27d2c801c396c793541e3f497055cc7d20
kirubelks <kirubel.n.gebeyehu@gmail.com>
2024-01-08 19:59:24 +0300

    edit for the controller of income

--- back/controllers/income.js
+++ back/controllers/income.js
@@ -14,11 +14,13 @@ exports.addIncome = async (req, res) => {
    try {
         //Validate
         if (!tittle || !category || !description || !date){
-            return res.status(400).json({error: 'all the fields are required.'})
+            return res.status(400).json({error: 'all the boxes need to be filled.'})
         }
         if (!amount <= 0 || !amount == 'number'){
-            return res.status(400).json({error: 'all the fields are required.'})
+            return res.status(400).json({error: 'Amount must be a posetive number and greater than 0.'})
         }
+        await income.save()
+        res.status.(200).json({message: 'Income Recorded'})
    } catch (error) {
     
    }
