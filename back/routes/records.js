72b85865891e4d986b32fc58e7e182930a8400e9
kirubelks <kirubel.n.gebeyehu@gmail.com>
2024-01-09 18:35:47 +0300

    t for the expense record entry

--- back/routes/records.js
+++ back/routes/records.js
@@ -5,4 +5,9 @@ const router = require('express').Router();
 router.post('/add-income', addIncome)
         .get('/get-incomes', getIncomes)
         .delete('/delete-income/:id', deleteIncomes)
+        .post('/add-expense', addExpense)
+        .get('/get-expense', getExpense)
+        .delete('/delete-expense/:id', deleteExpense)
+
+        
 module.exports = router
