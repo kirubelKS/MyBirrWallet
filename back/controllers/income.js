9d1ddda534043bf0b77bb91a79265d485b537e3c
kirubelks <kirubel.n.gebeyehu@gmail.com>
2024-01-09 18:30:57 +0300

    controller for the icome.js has been edited

--- back/controllers/income.js
+++ back/controllers/income.js
@@ -17,7 +17,7 @@ exports.addIncome = async (req, res) => {
             return res.status(400).json({error: 'all the boxes need to be filled.'})
         }
         if (amount <= 0 || !amount == 'number'){
-            return res.status(400).json({error: 'Amount must be a posetive number and greater than 0.'})
+            return res.status(400).json({error: 'Amount must be a positive number and greater than 0.'})
         }
         await income.save()
         res.status(200).json({message: 'income registered' })
