135d6105bae3ea4d9498f072710832dbdc0d82a9
kirubelks <kirubel.n.gebeyehu@gmail.com>
2024-01-09 18:05:14 +0300

    data entry for the income controller and routes.

--- back/controllers/income.js
+++ back/controllers/income.js
@@ -20,9 +20,30 @@ exports.addIncome = async (req, res) => {
             return res.status(400).json({error: 'Amount must be a posetive number and greater than 0.'})
         }
         await income.save()
-        res.status.(200).json({message: 'income registered' })
+        res.status(200).json({message: 'income registered' })
    } catch (error) {
-    
+    res.status(500).json({message: 'loading error' })
    }
    console.log(income)
+}
+
+
+exports.getIncomes = async (req, res) => {
+try {
+    const incomes = await IncomeSchema.find().sort({createdAt: -1})
+    res.status(200).json(incomes)
+} catch (error) {
+    res.status(500).json({message: 'Loading error'})
+}
+}
+
+exports.deleteIncomes = async (req, res) => {
+    const {id} = req.params;
+    IncomeSchema.findbyIdAndDelete(id)
+        .then((income) => {
+            res.status(200).json({message: 'Record deleted'})
+        })
+        .catch((error) =>{
+            res.status(500).json({message:'loading error'})
+        })
 }
--- back/routes/records.js
+++ back/routes/records.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 
 
 router.post('/add-income', addIncome)
-
+        .get('/get-incomes', getIncomes)
+        .delete('/delete-income/:id', deleteIncomes)
 module.exports = router
