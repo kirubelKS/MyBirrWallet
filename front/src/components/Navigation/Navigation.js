493ff88833c9b85d2a7402b1315c66b94c8a9d6b
kirubelks <kirubel.n.gebeyehu@gmail.com>
2024-01-14 19:08:50 +0300

    spelling changes

--- front/src/components/Navigation/Navigation.js
+++ front/src/components/Navigation/Navigation.js
@@ -11,8 +11,8 @@ function Navigation({active, setActive}) {
             <div className="user-con">
                 <img src={avatar} alt="" />
                 <div className="text">
-                    <h2>Mike</h2>
-                    <p>Your Money</p>
+                    <h2>kirubel</h2>
+                    <p>Your Balance</p>
                 </div>
             </div>
             <ul className="menu-items">
